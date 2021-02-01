c8a9b79699cd683ec8f4f50308df53086b15e03f
soroush-bagherian <soroushbagherian@yahoo.com>
2021-02-02 01:12:11 +0330

    add method getLessonSubject

--- www/sadaf/classes/subject.class.php
+++ www/sadaf/classes/subject.class.php
@@ -23,22 +23,24 @@ class Subject{
 		$ValueListArray = array();
 
 
-		$id = Subject::GetLastID();
+		$id = Subject::GetLastID("subject");
 		if($id==-1){$id=1;}
 		else{$id = $id+1;}
 
 		array_push($ValueListArray, $id); 
 		array_push($ValueListArray, $title); 
 		$mysql->Prepare($query);
-		$mysql->ExecuteStatement($ValueListArray);
-		
+		$res = $mysql->ExecuteStatement($ValueListArray);
+		if($rec = $res->fetch())
+		{
+			return true;
+		}
+		return -1;
     }
 
-
-    public static function GetLastID()
-	{
+    public static function GetLastID($table){
 		$mysql = pdodb::getInstance();
-        $query = "select max(id) from sadaf.subject";
+        $query = "select max(id) from sadaf.".$table;
 		$res = $mysql->Execute($query);
 		if($rec=$res->fetch())
 		{
@@ -65,30 +67,48 @@ class Subject{
 		$lesson="";
 		while($rec = $res->fetch())
 		{
-			$lesson .="<option  value='a'>". $rec['title'] ."</option>";
+			$lesson .="<option name='lesson'  value='".$rec['lessonid']."'>". $rec['title'] ."</option>";
 		}
 		return $lesson;
 	}
 
 	public static function setLessonSubject($lessonID , $subjectID){
-		// $userId = $_SESSION["UserID"];
-		// $ValueListArray = array();
+		$userId = $_SESSION["UserID"];
+		$ValueListArray = array();
 
-		// $mysql = pdodb::getInstance();
-		// $query = "select * from persons
-		// join person_lesson on persons.PersonID=person_lesson.personid
-		// join lesson on lesson.id = person_lesson.lessonid
-		// where persons.PersonID=1";
-		// $mysql->Prepare($query);
+		$mysql = pdodb::getInstance();
+		$query = "insert into subject_lesson (subjectId , lessonId) values(?,?);";
+		$mysql->Prepare($query);
 
-		// $res = $mysql->ExecuteStatement(array($userId));
+		$res = $mysql->ExecuteStatement(array($subjectID , $lessonID));
 
-		// $lesson="";
-		// while($rec = $res->fetch())
-		// {
-		// 	$lesson .="<option  value='a'>". $rec['title'] ."</option>";
-		// }
-		// return $lesson;
+		
+		if($rec = $res->fetch())
+		{
+			return true;
+		}
+		return -1;
+	}
+
+	public static function getLessonSubject($lessonID){
+		//$userId = $_SESSION["PersonID"];
+		$ValueListArray = array();
+		
+		$mysql = pdodb::getInstance();
+		$query = "select subject.title , subjectId from sadaf.subject
+		join sadaf.subject_lesson on subject.id = subject_lesson.subjectId
+		join sadaf.lesson on lesson.id = subject_lesson.lessonId
+		where lessonId = ?; ";
+		$mysql->Prepare($query);
+
+		$res = $mysql->ExecuteStatement(array($lessonID));
+
+		$lesson="";
+		while($rec = $res->fetch())
+		{
+			$lesson .="<option name='lesson'  value='".$rec['subjectId']."'>". $rec['title'] ."</option>";
+		}
+		return $lesson;
 	}
 
 
